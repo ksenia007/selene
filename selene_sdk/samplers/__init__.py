de7eafab93b863f86a03273ebf1f8925e30c24ef
ksenia007 <26440954+ksenia007@users.noreply.github.com>
2019-07-02 17:31:49 -0400

    Added normalization option

--- selene_sdk/samplers/__init__.py
+++ selene_sdk/samplers/__init__.py
@@ -6,7 +6,9 @@ from .sampler import Sampler
 from .online_sampler import OnlineSampler
 from .intervals_sampler import IntervalsSampler
 from .random_positions_sampler import RandomPositionsSampler
+from .random_positions_sampler_norm import RandomPositionsSamplerNorm
 from .random_positions_sampler_control import RandomPositionsSamplerControl 
+from .random_positions_sampler_matrix import RandomPositionsSamplerMatrix
 from .random_files_sampler import RandomFilesSampler
 from .multi_file_sampler import MultiFileSampler
 from . import file_samplers
@@ -15,7 +17,9 @@ __all__ = ["Sampler",
            "OnlineSampler",
            "IntervalsSampler",
            "RandomPositionsSampler",
+           "RandomPositionsSamplerNorm",
            "RandomFilesSamplerControl",
+           "RandomPositionsSamplerMatrix",
            "RandomFilesSampler",
            "MultiFileSampler",
            "file_samplers"]
